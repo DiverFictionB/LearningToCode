0c10bf59ec33508ce757220f0a244f199a910710
DiverFictionB <xboxsecret4@gmail.com>
2016-05-29 00:29:24 +0300

    Just practice coding

---
+++ LearningToCode/LearningToCode.cpp
@@ -0,0 +1,17 @@
+// LearningToCode.cpp : Defines the entry point for the console application.
+//
+#include<iostream>
+#include "stdafx.h"
+using namespace std;
+
+int main()
+{
+	 int Num1 = 10;
+	Num1 += 2;
+
+	cout << Num1 << endl;
+
+	system("pause");
+    return 0;
+}
+
